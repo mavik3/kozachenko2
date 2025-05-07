735da25e5cf3deed969b4d84984fcb937c672562
mavik3 <sasakozacenko885@gmail.com>
2025-05-07 15:19:45 +0200

    urobil som funkciu pre utvorenia matici aj ju odstranovanie

--- uloha2.c
+++ uloha2.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #define TRUE 1
 #define FALSE 0
 
@@ -7,4 +8,21 @@ typedef struct {
     int size;
     char transit;
 }NURELM;
+int **create_matica(int n){
+    int **matica = malloc(n * sizeof(int*));
+    for(int i = 0; i < n; i++){
+        matica[i] = calloc(n, sizeof(int));
+    }
+    return matica;
+}
 
+char nurelm_test_transitivity(NURELM *to_test){
+
+}
+
+void free_matica(int **matica, int n){
+    for(int i = 0; i < n; i++){
+        free(matica[i]);
+    }
+    free(matica);
+}
